071b2520fac21bc9a6a0f54f40d7a6849a7b2d14
br1lisboa <brlisbo@gmail.com>
2022-07-12 11:58:16 -0300

    finis contact skills and work

---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+        <form method='POST' action="https://getform.io/f/aa2bf1cb-6943-4ad6-ac75-aecff2abf3bc" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <p className='text-gray-300 py-4'>Submit the form belor or shot me an email - brlisbo@gmai.com</p>
+            </div>
+            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6] p-2' placeholder='Message' name="message" rows="10"></textarea>
+            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+        </form>
+    
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import ReactImg from '../assets/react.png';
+import Css from '../assets/css.png';
+import FireBase from '../assets/firebase.png';
+import GitHub from '../assets/github.png';
+import Html from '../assets/html.png';
+import JavaScript from '../assets/javascript.png';
+import TailWind from '../assets/tailwind.png';
+import Node from '../assets/node.png';
+
+function Skills() {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex-col justify-center w-full h-full'>
+        <div>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+          <p className='py-4 '>These are the technologies I've worked with</p>
+        </div>
+
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
+            <p className='my-4'>ReactJS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={JavaScript} alt="JS icon" />
+            <p className='my-4'>JavaScript</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Css} alt="CSS icon" />
+            <p className='my-4'>CSS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Html} alt="Html icon" />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={FireBase} alt="Firebase icon" />
+            <p className='my-4'>FireBase</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Node} alt="NodeJS icon" />
+            <p className='my-4'>NodeJS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={GitHub} alt="Github icon" />
+            <p className='my-4'>GitHub</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={TailWind} alt="Tailwind icon" />
+            <p className='my-4'>TailWind</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
