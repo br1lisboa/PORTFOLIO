991aa97858f4d2e9a40d60fdfc66a53c11c1afb6
br1lisboa <brlisbo@gmail.com>
2022-07-14 12:30:12 -0300

    added work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import EShop from '../assets/eShop.png';
 import Calc from '../assets/Calc.png';
 import Cart from '../assets/carWhitJS.png';
+import Todo from '../assets/todolist.png';
 
 function Work() {
   return (
@@ -26,6 +27,23 @@ function Work() {
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#EEF5DB] text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href="https://github.com/br1lisboa/lisboa_proyect_Ecommerce" target="_blank" rel='noreferrer'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#EEF5DB] text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage: `url(${Todo})`}} 
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* Hover effects */}
+                    <div className='text-center opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-[#EEF5DB] tracking-wider'>
+                            ToDoList whit ReactJS
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="https://todolistbybr1.netlify.app/" target="_blank" rel='noreferrer'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#EEF5DB] text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="https://github.com/br1lisboa/react_lista_de_tareas-thingsToDo-" target="_blank" rel='noreferrer'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#EEF5DB] text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
